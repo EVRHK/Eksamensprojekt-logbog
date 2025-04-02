fd64e6b5ab0bf9169e518daeee3ce92ac5d711e6
EVRHK <NEXT16276@edu.nextkbh.dk>
2025-04-02 17:48:44 +0200

    I gang med skud - Har billede

--- Kode/sketch.js
+++ Kode/sketch.js
@@ -1,8 +1,12 @@
 let imgSpaceship;
 let imgAlien;
 let imgBlueLaser;
-let blueLaser = {};
-let spaceship = {x: 350, y: 700, w: 100, h: 100, rightSpeed: 4, leftSpeed: 4, tempSpeed: 4};
+let imgBackground;
+let spaceship = {x: 350, y: 700, w: 100, h: 100, rightSpeed: 4, leftSpeed: 6, tempSpeed: 4};
+let blueLaser = {x: 0, y: 0, w: 0, h: 0, speed: 0};
+let bLaserCount = 0;
+let isSpacebarPressed = false;
+let bLaserArr = [];
 let alien = {};
 
 function preload(){
@@ -17,12 +21,21 @@ function setup()
 	createCanvas(800, 800);
 }
 
+function keyPressed(){
+	if(keyCode === 32){ // Spacebar
+		isSpacebarPressed = true;
+		console.log('sejt');
+	}
+}
+
 function draw()
 {
+	//Angiver koordinater til blueLaser
+	blueLaser = {x: spaceship.x + 23.5, y: spaceship.y - 75, w: 50, h: 100, speed: 6}
+	
 	//Loader baggrund og player
 	image(imgBackground, 0, 0, 800, 800);
 	spawnImage(imgSpaceship, spaceship);
-	spawnImage(imgBlueLaser, blueLaser);
 
 	//Bordercheck left
 	if (spaceship.x <= 0) {
@@ -44,9 +57,40 @@ function draw()
 	if(keyIsDown(65)){ // Key A
 		spaceship.x -= spaceship.leftSpeed;
 	}
+
+	//Skud
+	if(isSpacebarPressed == true){
+		console.log('meget' + ' ' + 'sejt');
+		bLaserCount = updateCount(bLaserCount, 1);
+		bLaserArr = newObjList(blueLaser, bLaserCount);
+
+		isSpacebarPressed = false;
+	}
+	
+	for(let i = 0; i<bLaserCount; i++){
+
+	}
 }
+
 
 
 function spawnImage(img, obj){
 	image(img, obj.x, obj.y, obj.w, obj.h);
+}
+
+function newObjList(obj, n){
+	let list = [];
+	for(let i = 0; i<n; i++){
+		list.push(obj)
+	}
+	return list;
+}
+
+function construct(obj){
+	return{...obj}
+}
+
+function updateCount(count, update){
+	count += update;
+	return count
 }
